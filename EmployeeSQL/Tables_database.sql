ba78f232cff7e7b5c7af35f6365166d6d6302e1d
scmo-ops <a01610125@exatec.tec.mx>
2021-05-22 19:01:42 -0500

    First commit

---
+++ EmployeeSQL/Tables_database.sql
@@ -0,0 +1,29 @@
+CREATE TABLE departments(
+    dept_no VARCHAR(30),
+    dept_name VARCHAR(30));
+
+CREATE TABLE dept_emp(
+    emp_no INT,
+    dept_no VARCHAR(30));
+
+CREATE TABLE dept_manager(
+    dept_no VARCHAR(30),
+    emp_no INT);
+
+CREATE TABLE employees(
+    emp_no INT,
+    emp_title_id VARCHAR(30),
+    birth_date DATE,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    sex VARCHAR(30),
+    hire_date DATE);
+
+CREATE TABLE salaries(
+    emp_no INT,
+    salary INT);
+
+CREATE TABLE titles(
+    title_id VARCHAR(30),
+    title VARCHAR(30),
+    emp_no INT);
